8eb37d77d34f0762b792dc9f3383f7e3aa1febe0
dejesusbg <dejesusbg5@gmail.com>
2025-05-05 23:03:05 -0500

    refactor: simplify NotFoundView by redirecting to home

--- native/app/+not-found.tsx
+++ native/app/+not-found.tsx
@@ -1,36 +1,6 @@
-import { Link, Stack } from 'expo-router';
-import { Text, StyleSheet } from 'react-native';
-import Theme, { ScreenView } from '~/components';
+import { useRouter } from 'expo-router';
 
 export default function NotFoundView() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'oops!' }} />
-      <ScreenView title="not found">
-        <Text style={styles.title}>this route doesn't exist</Text>
-        <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>navigate home</Text>
-        </Link>
-      </ScreenView>
-    </>
-  );
+  const router = useRouter();
+  router.push('/');
 }
-
-const styles = StyleSheet.create({
-  title: {
-    color: Theme.white,
-    fontSize: 24,
-    fontWeight: 600,
-    textAlign: 'center',
-    marginTop: 12,
-  },
-  link: {
-    textAlign: 'center',
-    marginTop: 12,
-  },
-  linkText: {
-    fontWeight: 500,
-    fontSize: 16,
-    color: Theme.link,
-  },
-});
