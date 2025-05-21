90921c1315e050f1aa99103a1aceb042b0c8fc9b
dejesusbg <dejesusbg5@gmail.com>
2025-05-20 23:41:26 -0500

    refactor: simplify by removing BaseText wrapper

--- native/components/layout/Text.tsx
+++ native/components/layout/Text.tsx
@@ -1,65 +1,46 @@
-import {
-  Text as RNText,
-  TextInput as RNTextInput,
-  StyleSheet,
-  TextInputProps,
-  TextProps,
-  TextStyle,
-  View,
-} from 'react-native';
+import { Text as RNText, StyleSheet, TextProps, TextStyle, View } from 'react-native';
 import Theme from '~/components/theme';
 import usePreferencesContext from '~/context/PreferencesProvider';
 import { getLocale } from '~/services';
 import t from '~/utils/text';
 
-interface BaseTextProps extends TextProps {
-  TextComponent: React.ElementType;
-}
+/**
+ * Renders a custom text component that adapts its style and content based on user preferences.
+ *
+ * - If user preferences are not loaded, displays a skeleton placeholder.
+ * - Applies a dynamic font family and letter spacing based on the provided style's fontWeight.
+ * - If the children prop is a string, it is translated using the user's locale preferences.
+ *
+ * @param children - The content to display inside the text component. If a string, it will be translated.
+ * @param style - Optional style object to customize the text appearance.
+ * @param props - Additional props passed to the underlying RNText component.
+ * @returns A styled text component or a skeleton placeholder if preferences are not available.
+ */
+export function Text({ children, style, ...props }: TextProps) {
+  const { preferences } = usePreferencesContext();
 
-function BaseText({ TextComponent, children, style, ...props }: BaseTextProps) {
   const styles = StyleSheet.create({
     text: {
       fontFamily: 'Inter-' + ((style as TextStyle)?.fontWeight || '500'),
       letterSpacing: parseInt((style as TextStyle)?.fontWeight as string) > 500 ? -0.75 : -0.25,
     },
+    skeleton: { backgroundColor: Theme.container, borderRadius: 16 },
+    skeletonChildren: { opacity: 0 },
   });
-
-  return (
-    <TextComponent style={[styles.text, style]} {...props}>
-      {children}
-    </TextComponent>
-  );
-}
-
-export function Text({ children, ...props }: TextProps) {
-  const { preferences } = usePreferencesContext();
 
   if (preferences === null) {
     return (
       <View style={[styles.skeleton]}>
         <View style={[styles.skeletonChildren]}>
-          <BaseText TextComponent={RNText}>{children}</BaseText>
+          <RNText>{children}</RNText>
         </View>
       </View>
     );
   }
 
   return (
-    <BaseText TextComponent={RNText} {...props}>
+    <RNText style={[styles.text, style]} {...props}>
       {typeof children === 'string' ? t(children, getLocale(preferences.spanish!)) : children}
-    </BaseText>
+    </RNText>
   );
 }
-
-export function TextInput({ children, ...props }: TextInputProps) {
-  return (
-    <BaseText TextComponent={RNTextInput} {...props}>
-      {children}
-    </BaseText>
-  );
-}
-
-const styles = StyleSheet.create({
-  skeleton: { backgroundColor: Theme.container, borderRadius: 16 },
-  skeletonChildren: { opacity: 0 },
-});
